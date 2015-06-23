abe22a6212e5e2361d10bb0c6b3a47adf5f38bfd
Sizhao <zhangsizhao@win-trust.cn>
2015-06-23 17:54:41 +0800

    extjs

---
+++ www2/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>index</title>
+</head>
+<body>
+  <a href = "orderForm.html" >Get in Order Form</a>
+</body>
+</html>
---
+++ www2/process_order.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+  <title>My Order Result (PhpTest)</title>
+</head>
+<body>
+<h1>Own Php Test</h1>
+<h2>Order Result</h2>
+<?php
+  echo "php Part:order Process"
+?>
+</body>
+</html>
